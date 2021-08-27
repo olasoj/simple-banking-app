871aae9a5fc2b53a1aab3e997af1ab2b11b4811e
olasoj <olasojiige@gmail.com>
2021-08-27 20:01:19 +0100

    Completed JWT module.

---
+++ src/main/java/com/interview/question/jwt/config/JwtConfig.java
@@ -0,0 +1,17 @@
+package com.interview.question.jwt.config;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@NoArgsConstructor
+public class JwtConfig {
+    @Value("${app.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${app.jwt.expire-length}")
+    private int expireLength;
+}

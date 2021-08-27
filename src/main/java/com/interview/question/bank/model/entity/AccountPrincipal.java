bea016751c4f764287797992de0e2025a197176c
olasoj <olasojiige@gmail.com>
2021-08-27 19:57:21 +0100

    Configured web security config. Completed account module.

---
+++ src/main/java/com/interview/question/bank/model/entity/AccountPrincipal.java
@@ -0,0 +1,54 @@
+package com.interview.question.bank.model.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+
+
+@Component
+public class AccountPrincipal implements UserDetails, Serializable {
+    private final Account account;
+
+    public AccountPrincipal(Account account) {
+        this.account = account;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return account.getAccountPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getAccountName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/interview/question/bank/repository/AccountRepository.java
@@ -0,0 +1,37 @@
+package com.interview.question.bank.repository;
+
+import com.interview.question.bank.model.entity.Account;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.TreeMap;
+
+@Component
+public class AccountRepository {
+    private static final Map<String, Account> accounts = new TreeMap<>();
+
+    public void save(Account account) {
+        accounts.put(account.getAccountNumber(), account);
+    }
+
+    public void update(Account account) {
+        accounts.put(account.getAccountNumber(), account);
+    }
+
+    public Optional<Account> findByAccountNumber(String accountNumber) {
+        return Optional.ofNullable(accounts.get(accountNumber));
+    }
+
+    public Map<String, Account> findAll() {
+        return accounts;
+    }
+
+    public boolean accountNameExists(String accountName) {
+        for (Map.Entry<String, Account> account : accounts.entrySet())
+            if (accounts.get(account.getKey()).getAccountName().equals(accountName))
+                return true;
+        return false;
+    }
+
+}
